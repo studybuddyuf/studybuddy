dc68ed9cbd1135161752dba078e72352fadce2b7
Dan D'Andrea <dan.dandrea@gmail.com>
2013-09-22 16:24:18 -0400

    Changed paths in settings.py

--- django/studybuddy/settings.py
+++ django/studybuddy/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/dandandrea/studybuddy/studybuddy.db', # Or path to database file if using sqlite3.
+        'NAME': '<CHANGE TO ROOT OF YOUR STUDYBUDDY GIT REPO>/studybuddy.db', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/dandandrea/studybuddy/django/studybuddy/templates',
+    '<CHANGE TO ROOT OF YOUR STUDYBUDDY GIT REPO>/django/studybuddy/templates',
 )
 
 INSTALLED_APPS = (
