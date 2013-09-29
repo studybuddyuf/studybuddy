dbf7d079e4b38ca3938f6337d8bd83eace965d0d
dandandrea <dan.dandrea@gmail.com>
2013-09-29 11:12:09 -0400

    Delete views.py

--- django/studybuddy/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from studybuddy.models import User
-
-def hello(request):
-    user = User.objects.all()[0]
-    return render(request, 'hello.html', {'user': user})
