e33886d81024d985b5fce7b63ac8343dd4b6c786
Dan D'Andrea <dan.dandrea@gmail.com>
2013-09-29 11:52:56 -0400

    Added user, extending from auth's user

--- django/studybuddy/profilePage/models.py
+++ django/studybuddy/profilePage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class StudyBuddyUser(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=10)
+    school_name = models.charField(max_length=100)
+    year = models.PositiveSmallIntegerField()
