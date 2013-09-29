d9e2dcf8fbb48a8abea49b0c31c77fd4ce10f680
Dan D'Andrea <dan.dandrea@gmail.com>
2013-09-29 11:23:17 -0400

    Working on settings.py

--- django/studybuddy/studyBuddy/settings.py
+++ django/studybuddy/studyBuddy/settings.py
@@ -1,5 +1,7 @@
 # Django settings for studyBuddy project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -14,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.join.path(PROJECT_DIRECTORY, 'studyBuddy.db'), # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_DIRECTORY, 'studyBuddy.db'), # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
