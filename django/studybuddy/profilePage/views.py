172f4e83b87ed69ddcb16a517c545d735e3df7a0
maximus0021 <Vaderu18174@gmail.com>
2013-10-03 18:16:32 -0400

    Update 	User is automatically log in when registering

--- django/studybuddy/profilePage/views.py
+++ django/studybuddy/profilePage/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.contrib import auth
+from django.contrib.auth import authenticate, login
 from django.core.context_processors import csrf
 from forms import MyRegistrationForm
 
@@ -16,7 +17,9 @@ def register_user(request):
 	if request.method =="POST":
 		form = MyRegistrationForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_user = form.save()
+			new_user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+			auth.login(request, new_user)
 			return HttpResponseRedirect('/home/')
 
 	args = {}
