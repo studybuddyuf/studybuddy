873e68ee98b44667593239c8d14ce5d9149d5eb8
Mike <wiz@ubuntu.(none)>
2013-10-12 13:13:51 -0400

    Implemented model for user and schedule

--- django/studybuddy/startPage/admin.py
+++ django/studybuddy/startPage/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from profilePage.models import StudyBuddyUser
+from profilePage.models import *
 
 admin.site.register(StudyBuddyUser)
+admin.site.register(UserSchedule)
+admin.site.register(Schedule)
+admin.site.register(CourseSection)
+admin.site.register(CourseName)
+admin.site.register(Semester)
+
