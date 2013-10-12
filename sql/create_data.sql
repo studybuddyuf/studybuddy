5e9bc0814a64309a6e6f675983c2667a2b79a4b0
Mike <wiz@ubuntu.(none)>
2013-10-12 13:24:28 -0400

    Added SQL script for creating our test data

---
+++ sql/create_data.sql
@@ -0,0 +1,12 @@
+# Here is how to run this script from the "sql" directory from the command line:
+#
+# sqlite3 -init create_data.sql ../django/studybuddy/studyBuddy.db
+
+# Semester table
+INSERT INTO profilePage_semester (semester, startDate, endDate) VALUES ('Spring 2013', '01-01-2013', '05-31-2013');
+INSERT INTO profilePage_semester (semester, startDate, endDate) VALUES ('Summer 2013', '06-01-2013', '07-31-2013');
+INSERT INTO profilePage_semester (semester, startDate, endDate) VALUES ('Fall 2013', '08-01-2013', '12-31-2013');
+
+# Another table
+
+# Yet another table
