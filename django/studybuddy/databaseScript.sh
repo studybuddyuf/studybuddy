687a7acaf5bb0f431a52fcccbda1433d5dab9d63
Dan D'Andrea <dan.dandrea@gmail.com>
2013-10-19 13:13:55 -0400

    Fixed dates/times in database script so that they dont give a timezone warning

--- django/studybuddy/databaseScript.sh
+++ django/studybuddy/databaseScript.sh
@@ -62,7 +62,7 @@ cs = CourseSection(courseID=cn,semester=fall13,regularScheduleID=sch4, discussio
 cs.save()
 
 #makeing 5 AUTH_USERS (not a studyBuddy User)
-user1 = User(id=1, username='vanwirer', first_name='Vanwirer', last_name='Griffin', email='hackYou@gmail.com',last_login='2013-12-12 08:15:16', is_superuser=1, is_staff=1, is_active=1, date_joined='2013-10-10')
+user1 = User(id=1, username='vanwirer', first_name='Vanwirer', last_name='Griffin', email='hackYou@gmail.com',last_login='2013-12-12 08:15:16+00:00', is_superuser=1, is_staff=1, is_active=1, date_joined='2013-10-10 00:00:00+00:00')
 user1.set_password('pass')
 user1.save()
 sbu1 = StudyBuddyUser(user_id=1,phone = 1234567891,school_name = 'University of Florida', year = 5)
@@ -76,7 +76,7 @@ usi = UserSchedule(userID=sbu1,scheduleID=sch3)
 usi.save()
 
 
-user2 = User(id=2, username='user2', first_name='firstUser2', last_name='lastUser2', email='user2@gmail.com',last_login='2013-12-12 08:15:16', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10')
+user2 = User(id=2, username='user2', first_name='firstUser2', last_name='lastUser2', email='user2@gmail.com',last_login='2013-12-12 08:15:16+00:00', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10 00:00:00+00:00')
 user2.set_password('pass')
 user2.save()
 sbu2 = StudyBuddyUser(user_id=2,phone = 1234567891,school_name = 'University of Florida', year = 5)
@@ -91,19 +91,19 @@ usi = UserSchedule(userID=sbu2,scheduleID=sch5)
 usi.save()
 
 
-user3 = User(id=3, username='user3', first_name='firstUser3', last_name='lastUser3', email='user3@gmail.com',last_login='2013-12-12 08:15:16', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10')
+user3 = User(id=3, username='user3', first_name='firstUser3', last_name='lastUser3', email='user3@gmail.com',last_login='2013-12-12 08:15:16+00:00', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10 00:00:00+00:00')
 user3.set_password('pass')
 user3.save()
 sbu3 = StudyBuddyUser(user_id=3,phone = 1234567891,school_name = 'University of Florida', year = 5)
 sbu3.save()
 
-user4 = User(id=4, username='user4', first_name='firstUser4', last_name='lastUser4', email='user4@gmail.com',last_login='2013-12-12 08:15:16', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10')
+user4 = User(id=4, username='user4', first_name='firstUser4', last_name='lastUser4', email='user4@gmail.com',last_login='2013-12-12 08:15:16+00:00', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10 00:00:00+00:00')
 user4.set_password('pass')
 user4.save()
 sbu4 = StudyBuddyUser(user_id=4,phone = 1234567891,school_name = 'University of Florida', year = 5)
 sbu4.save()
 
-user5 = User(id=5, username='user5', first_name='firstUser5', last_name='lastUser5', email='user5@gmail.com',last_login='2013-12-12 08:15:16', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10')
+user5 = User(id=5, username='user5', first_name='firstUser5', last_name='lastUser5', email='user5@gmail.com',last_login='2013-12-12 08:15:16+00:00', is_superuser=0, is_staff=0, is_active=1, date_joined='2013-10-10 00:00:00+00:00')
 user5.set_password('pass')
 user5.save()
 sbu5 = StudyBuddyUser(user_id=5,phone = 1234567891,school_name = 'University of Florida', year = 5)
