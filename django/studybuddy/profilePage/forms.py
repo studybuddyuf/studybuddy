bc1a55b2ec23f5bfe2a2fbcede5009bb0936cf8b
Ryan <bedinski13@gmail.com>
2013-10-26 16:33:42 -0400

    Added form placeholder text in edit profile

--- django/studybuddy/profilePage/forms.py
+++ django/studybuddy/profilePage/forms.py
@@ -2,8 +2,11 @@ from django import forms
 from models import StudyBuddyUser
 
 class UserProfileForm(forms.ModelForm):
-	email = forms.EmailField()
-	about_me = forms.CharField(widget=forms.Textarea(attrs={'class' : 'about_me'}))
+	phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Enter your phone number'}))
+	email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder' : 'Enter your email address'}))
+	school_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Enter your school name'}))
+	year = forms.CharField(widget=forms.TextInput(attrs={'placeholder' : 'Enter your school year'}))
+	about_me = forms.CharField(widget=forms.Textarea(attrs={'class' : 'about_me' , 'placeholder' : 'Enter something about yourself'}))
     	class Meta:
         	model = StudyBuddyUser
         	fields = ['phone', 'email', 'school_name', 'year', 'about_me']
